a810424c0a73d505879d65906bf1fede63ed91b2
maliming <malimings@gmail.com>
2018-01-21 20:13:31 +0800

    Add project files.

---
+++ ADC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
